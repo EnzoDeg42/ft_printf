8117e007bfdd609f5a4873d60e7e79345ece4120
Enzo Degraeve <enzodeg40@gmail.com>
2024-01-19 18:29:16 +0100

    Import from intra of 42

---
+++ libft/src/ft_memmove.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: edegraev <edegraev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/07 10:41:15 by edegraev          #+#    #+#             */
+/*   Updated: 2023/11/14 13:53:23 by edegraev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+	size_t			i;
+
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
+	i = 0;
+	if (d == s)
+		return (dest);
+	if (d < s)
+		ft_memcpy(d, s, n);
+	else
+	{	
+		while (n > 0)
+		{
+			d[n - 1] = s[n - 1];
+			n--;
+		}
+	}
+	return (dest);
+}
+
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// int main()
+// {
+// 	int size = 20;
+// 	char *src = "Hello World!";
+// 	char *dest = malloc(sizeof(char) * size);
+// 	ft_memmove(dest, src, size);
+// 	printf("%s\n", dest);
+// 	free(dest);
+// 	return (0);
+// }
